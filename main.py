661f638049dc954e4df068a72b12776bc5d57af3
neryr <nery.reyesrt@hotmail.com>
2022-08-11 15:20:32 -0500

    Strings y el comando Update

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ def list_clients():
     print(clients)
 
 
+def update_client(client_name, updated_client_name):
+    global clients
+    if client_name  in clients:
+        clients = clients.replace(client_name + ', ', updated_client_name + ', ')
+    else:
+        print('Client is not in clients list')
+
+
 def _add_comma():
     global clients
     clients += ", "
@@ -25,18 +33,28 @@ def _print_welcome():
     print('*' * 50)
     print('What would you like to do today?')
     print('[C] Create client')
+    print('[U] Update client')
     print('[D] Delete client')
+
+def _get_client_name():
+    return input('What\'s the client name? ')
 
 
 if __name__ == "__main__":
     _print_welcome()
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name? ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
         pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What\'s the updated client name? ')
+        update_client(client_name, updated_client_name)
+        list_clients()
     else:
         print('Invalid command')
