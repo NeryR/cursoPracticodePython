81f51a797fbf05568e9543440b02e81b990bd571
neryr <nery.reyesrt@hotmail.com>
2022-08-11 15:38:37 -0500

    Delete

--- main.py
+++ main.py
@@ -23,6 +23,14 @@ def update_client(client_name, updated_client_name):
         print('Client is not in clients list')
 
 
+def delete_client(client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ', ', '')
+    else:
+        print('Client is not in clients list')
+
+
 def _add_comma():
     global clients
     clients += ", "
@@ -50,7 +58,9 @@ if __name__ == "__main__":
         create_client(client_name)
         list_clients()
     elif command == 'D':
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
     elif command == 'U':
         client_name = _get_client_name()
         updated_client_name = input('What\'s the updated client name? ')
