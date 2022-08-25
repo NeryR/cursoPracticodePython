bb31fa1081c28cde8a60baf7ef1864d21e6c8745
neryr <nery.reyesrt@hotmail.com>
2022-08-25 17:54:26 -0500

    Diccionarios

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+from pydoc import cli
 import sys
+from turtle import position
 
 
-clients = ['Pablo', 'Ricardo']
+clients = [
+    {
+        'name': 'Pablo',
+        'company': 'Google', 
+        'email': 'pablo@google.com',
+        'position': 'Software Engineer',
+    },
+    {
+        'name': 'Ricardo',
+        'company': 'Facebook', 
+        'email': 'ricardo@facebook.com',
+        'position': 'Data Engineer',
+    }
 
-def create_client(client_name):
+
+]
+
+def create_client(client):
     # Con el comando global se puede acceder a las variables externas dentro de una función
     global clients
-    if client_name not in clients:
-        clients.append(client_name)
+    if client not in clients:
+        clients.append(client)
     else: 
         print('Client already is in the client\' list')
 
@@ -22,24 +39,29 @@ def search_client(client_name):
 
 def list_clients():
     for idx, client in enumerate(clients):
-        print('{}: {}'.format(idx, client))
+        print('{uid} | {name} | {company} | {email} | {position}'.format(
+            uid = idx,
+            name = client['name'],
+            company = client['company'],
+            email = client['email'],
+            position = client['position']
+        ))
 
 
-def update_client(client_name, updated_client_name):
+def update_client(client_id, updated_client):
     global clients
-    if client_name  in clients:
-        index = clients.index(client_name)
-        clients[index] = updated_client_name
+    if len(clients) - 1 >= client_id:
+        clients[client_id] = updated_client
     else:
-        print('Client is not in clients list')
+        print('Client not in client\'s list')
 
 
-def delete_client(client_name):
+def delete_client(client_id):
     global clients
-    if client_name in clients:
-        clients.remove(client_name)
-    else:
-        print('Client is not in clients list')
+    for idx, client in enumerate(clients):
+        if idx == client_id:    
+            del clients[idx]
+            break
 
 
 def _print_welcome():
@@ -51,6 +73,23 @@ def _print_welcome():
     print('[U] Update client')
     print('[D] Delete client')
     print('[S] Search client')
+
+def _get_client_field(field_name):
+    field = None
+
+    while not field:
+        field = input('What is the client {}? '.format(field_name))
+
+    return field
+
+def _get_client_from_user():
+    client = {
+        'name': _get_client_field('name'),
+        'company': _get_client_field('company'),
+        'email': _get_client_field('email'),
+        'position': _get_client_field('position'),
+    }
+    return client
 
 def _get_client_name():
     client_name = None
@@ -70,27 +109,30 @@ if __name__ == "__main__":
     command = command.upper()
 
     if command == 'C':
-        client_name = _get_client_name()
-        create_client(client_name)
+        client = _get_client_from_user()
+
+        create_client(client)
         list_clients()
     elif command == 'L':
         list_clients()
     elif command == 'D':
-        client_name = _get_client_name()
-        delete_client(client_name)
+        client_id = int(_get_client_field('id'))
+
+        delete_client(client_id)
         list_clients()
     elif command == 'U':
-        client_name = _get_client_name()
-        updated_client_name = input('What\'s the updated client name? ')
-        update_client(client_name, updated_client_name)
+        client_id = int(_get_client_field('id'))
+        updated_client = _get_client_from_user()
+
+        update_client(client_id, updated_client)
         list_clients()
     elif command == "S":
-        client_name = _get_client_name()
+        client_name = _get_client_field('name')
         found = search_client(client_name)
 
         if found:
             print('The client is in the client\'s list')
         else:
-            print('The client: {} is not in the client\'s list'.format(client_name))
+            print('The client {} is not in our client\'s list'.format(client_name))
     else:
         print('Invalid command')
