b2b8bf2417a620064f38c8d10a89d9a3fdf00656
neryr <nery.reyesrt@hotmail.com>
2022-08-29 13:23:21 -0500

    Decoradores

---
+++ decorators.py
@@ -0,0 +1,32 @@
+PASSWORD = '12345'
+
+
+def password_required(func):
+    def wrapper():
+        password = input('Cuál es tu contraseña? ')
+
+        if password == PASSWORD:
+            return func()
+        else:
+            print('La contraseña es incorrecta.')
+
+    return wrapper
+
+@password_required
+def need_password():
+    print('La contraseña es correcta')
+
+def upper(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        return result.upper()
+    return wrapper
+
+@upper
+def say_my_name(name):
+    return 'Hola, {}'.format(name)
+
+if __name__ == '__main__':
+    print(say_my_name('Nery'))
+    #need_password()
