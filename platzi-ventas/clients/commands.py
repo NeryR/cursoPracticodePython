32b25020982343297171c17966b0435297783be1
neryr <nery.reyesrt@hotmail.com>
2022-08-30 15:52:29 -0500

    Actualización CRUD

--- platzi-ventas/clients/commands.py
+++ platzi-ventas/clients/commands.py
@@ -1,4 +1,8 @@
 import click
+from tabulate import tabulate
+
+from clients.services import ClientService
+from clients.models import Client
 
 @click.group()
 def clients():
@@ -6,17 +10,38 @@ def clients():
 
 
 @clients.command()
+@click.option('-n', '--name', type=str, prompt=True, help='The client name')
+@click.option('-c', '--company', type=str, prompt=True, help='The client company')
+@click.option('-e', '--email', type=str, prompt=True, help='The client email')
+@click.option('-p', '--position', type=str, prompt=True, help='The client position')
 @click.pass_context
 def create(ctx, name, company, email, position):
     """Create new client"""
-    pass
+    client = Client(name, company, email, position)
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_service.create_client(client)
 
 
 @clients.command()
 @click.pass_context
 def  list(ctx):
     """List clients"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+    clients_list = client_service.list_clients()
+
+    headers = [field.capitalize() for field in Client.schema()]
+    table = []
+
+    for client in clients_list:
+        table.append(
+            [client['name'],
+            client['company'],
+            client['email'],
+            client['position'],
+            client['uid']]
+        )
+    print(tabulate(table, headers))
 
 
 @clients.command()
